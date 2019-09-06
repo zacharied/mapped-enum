9e89b167ed07dec62cdaca8ccc9968e76fc7977e
Zach Day <zachday@Zachs-iMac.local>
2019-09-06 13:59:15 -0400

    Add documentation and replace kwargs

--- mapped_enum/__init__.py
+++ mapped_enum/__init__.py
@@ -1,15 +1,35 @@
 from enum import Enum
 import re
 
-KWARGS_PARAM_TO_PREFIX = 'to_prefix'
-KWARGS_PARAM_FROM_PREFIX = 'from_prefix'
-
 identifier_regex = re.compile(r'[A-Za-z_][A-Za-z0-9_]*')
 map_key_regex = re.compile(r'[A-Za-z0-9_]+')
 
 
-def enum_map(keys, **kwargs):
-    # TODO Documentation
+def enum_map(keys, to_prefix='to_', from_prefix='from_'):
+    """
+    Map the values of enum members to keywords for clear and concise conversions to and lookups from those keys. Each
+    enum member must have a tuple value with a length equal to the number of space-separated keywords defined in the
+    `keys` parameter.
+
+    For each space-separated key specified in the `keys` parameter, a method called `to_<key>` and a class method called
+    `from_<key>` will be added to the enum. These convert to and from the enum values at the index of the keyword. For
+    example, given a mapped enum `Events` created with keys `'day month'` and a single member `BIRTHDAY = 11, 'august'`,
+    a call to `Events.BIRTHDAY.to_day()` would return `11` and `Events.BIRTHDAY.to_month()` would return `'august'`.
+    Likewise, a call to `Events.from_day(11)` would return `Events.BIRTHDAY`. Details on the generated methods are
+    provided below.
+
+    `to_<key>` is called on an enum member and takes no arguments. It returns the indexed member of the enum value tuple
+    corresponding to the index of the `key`word.
+
+    `from_<key>` is called directly on the enum class. It takes a single argument, the value to search for enum members
+    with the value index corresponding to the index of the `key`word. If there is no enum member that satisfies the
+    requested value, it will return `None`.
+
+    :param keys: A single string containing a space-separated series of keys.
+    :param to_prefix: An alternative prefix for the conversion methods instead of `to_`.
+    :param from_prefix: An alternative prefix for the lookup methods instead of `from_`.
+    :return: The decorated class.
+    """
     keys = keys.replace('-', '_').split(' ')
 
     if keys[0] == '':
@@ -20,16 +40,11 @@ def enum_map(keys, **kwargs):
             raise ValueError(f'key {keyname} results in invalid identifiers')
 
     def inner(cls):
-        if KWARGS_PARAM_TO_PREFIX in kwargs:
-            if identifier_regex.match(kwargs[KWARGS_PARAM_TO_PREFIX]) is None:
-                raise ValueError(f'invalid prefix {kwargs[KWARGS_PARAM_TO_PREFIX]} for `to` method')
-        if KWARGS_PARAM_FROM_PREFIX in kwargs:
-            if identifier_regex.match(kwargs[KWARGS_PARAM_FROM_PREFIX]) is None:
-                raise ValueError(f'invalid prefix {kwargs[KWARGS_PARAM_FROM_PREFIX]} for `from` method')
-
-        to_prefix = 'to_' if KWARGS_PARAM_TO_PREFIX not in kwargs else kwargs[KWARGS_PARAM_TO_PREFIX]
-        from_prefix = 'from_' if KWARGS_PARAM_FROM_PREFIX not in kwargs else kwargs[KWARGS_PARAM_FROM_PREFIX]
-
+        # Base constraint checking.
+        if identifier_regex.match(to_prefix) is None:
+            raise ValueError(f'invalid prefix "{to_prefix}" for `to` method')
+        if identifier_regex.match(from_prefix) is None:
+            raise ValueError(f'invalid prefix "{from_prefix}" for `from` method')
         if not issubclass(cls, Enum):
             raise ValueError(f'{cls} is not a descendant of Enum')
 
